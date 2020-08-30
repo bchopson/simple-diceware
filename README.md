d5183c80a390b5956005e510d79b46c28c83586b
Ben Chopson <bchopson@users.noreply.github.com>
2020-08-30 17:56:08 -0400

    Add README, change wordlist

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple Diceware
+
+Create diceware-style passwords from the command line. 
+
+Wordlist copied from https://www.eff.org/files/2016/07/18/eff_large_wordlist.txt
+
+### Install
+
+`pip install --user simple-diceware`
+
+### Use
+
+`diceware 6` to generate six-word passphrase
--- simple_diceware/simple_diceware.py
+++ simple_diceware/simple_diceware.py
@@ -5,7 +5,7 @@ from typing import Dict, List
 
 
 def get_list() -> Dict[str, str]:
-    wordlist = pkgutil.get_data(__name__, 'diceware.wordlist.asc').decode('utf-8')
+    wordlist = pkgutil.get_data(__name__, 'eff_large_wordlist.txt').decode('utf-8')
     return {
         number: phrase
         for [number, phrase] in [line.strip().split('\t') for line in wordlist.strip().split('\n')]
@@ -28,9 +28,15 @@ def generate_password(length: int) -> str:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('length', nargs=1, type=int)
+    parser.add_argument(
+        'length',
+        nargs='?',
+        default=6,
+        type=int,
+        help='Number of words in the passphrase (defaults to 6)',
+    )
     parsed = parser.parse_args()
-    print(generate_password(parsed.length[0]))
+    print(generate_password(parsed.length))
 
 
 if __name__ == '__main__':
